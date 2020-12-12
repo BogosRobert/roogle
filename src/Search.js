055a6f3a2c155c9ed2fe6f2ba0cd112763efb20b
BogosRobert <robert.bogos66@gmail.com>
2020-12-12 23:03:34 +0200

    Stopped hebe because context API

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ import MicIcon from '@material-ui/icons/Mic';
 import Button from '@material-ui/core/Button';
 import { useHistory } from "react-router-dom";
 
-function Search() {
+function Search( { hideButtons = false } ) {
 
     const [input, setInput] = useState("");
     const history = useHistory();
@@ -27,10 +27,19 @@ function Search() {
                 <MicIcon />
             </div>
 
-            <div className="search__buttons">
-                <Button type="submit" variant="outlined" onClick={search}>Roogle Search</Button>
-                <Button variant="outlined">I'm feeling lucky</Button>
-            </div>
+            {!hideButtons ? (
+                <div className="search__buttons">
+                    <Button type="submit" variant="outlined" onClick={search}>Roogle Search</Button>
+                    <Button variant="outlined">I'm feeling lucky</Button>
+                </div> ) : (
+                <div className="search__buttons">
+                    <Button className="search__buttonsHidden" type="submit" variant="outlined" onClick={search}>Roogle Search</Button>
+                    <Button className="search__buttonsHidden" variant="outlined">I'm feeling lucky</Button>
+                </div> )
+                
+            }
+
+            
 
         </form>
     )
